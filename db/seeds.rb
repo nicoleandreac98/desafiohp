13e33bbcb25546d4e924c14ba3d44bd6d470fd83
Nicole <yumi.cch.du@gmail.com>
2023-06-27 22:13:34 +0000

    subiendo app

---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+10.times do
+    Character.create(
+        name: Faker::Movies::HarryPotter.character,
+        location: Faker::Movies::HarryPotter.location,
+        house: Faker::Movies::HarryPotter.house
+    )
+end
+
